4f34d0a019c5009d19ee31ca2ea25e7e7202a62f
Robert Jaufmann <robert.jaufmann@allianz.de>
2018-06-06 08:17:53 +0200

    Fixed @Param - Tag

--- src/main/java/de/jaufmann/mosterei/AnmeldungRepository.java
+++ src/main/java/de/jaufmann/mosterei/AnmeldungRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import de.jaufmann.mosterei.model.Anmeldungen;
@@ -14,6 +15,6 @@ import de.jaufmann.mosterei.model.Anmeldungen;
 public interface AnmeldungRepository extends JpaRepository<Anmeldungen, Long> {
 
     @Query(value = "SELECT * FROM anmeldungen a where a.beginn BETWEEN :start AND :ende", nativeQuery = true)
-    List<Anmeldungen> findAllByDate(Date start, Date ende);
+    List<Anmeldungen> findAllByDate(@Param("start") Date start, @Param("ende") Date ende);
 
 }
